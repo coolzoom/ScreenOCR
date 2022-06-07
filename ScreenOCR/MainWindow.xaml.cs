87ba02b51a1db51b33381866691a3240894c365a
zz <zz@test.com>
2022-06-07 14:57:07 +0800

    Update MainWindow.xaml.cs

--- ScreenOCR/MainWindow.xaml.cs
+++ ScreenOCR/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using Clipboard = System.Windows.Clipboard;
 using Point = System.Windows.Point;
 using System.Text;
 using System.Runtime.InteropServices;
+using System.Windows.Media;
 
 namespace ScreenOCR
 {
@@ -144,11 +145,71 @@ namespace ScreenOCR
 			public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
 		}
 
+		/// <summary>
+		/// Helper class containing Gdi32 API functions
+		/// </summary>
+		private class GDI32
+		{
+
+			public const int SRCCOPY = 0x00CC0020; // BitBlt dwRop parameter
+			[DllImport("gdi32.dll")]
+			public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest,
+				int nWidth, int nHeight, IntPtr hObjectSource,
+				int nXSrc, int nYSrc, int dwRop);
+			[DllImport("gdi32.dll")]
+			public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth,
+				int nHeight);
+			[DllImport("gdi32.dll")]
+			public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+			[DllImport("gdi32.dll")]
+			public static extern bool DeleteDC(IntPtr hDC);
+			[DllImport("gdi32.dll")]
+			public static extern bool DeleteObject(IntPtr hObject);
+			[DllImport("gdi32.dll")]
+			public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
+		}
+		/// <summary>
+		/// Creates an Image object containing a screen shot of a specific window
+		/// </summary>
+		/// <param name="handle">The handle to the window. (In windows forms, this is obtained by the Handle property)</param>
+		/// <returns></returns>
+		public Image CaptureWindow(IntPtr handle)
+		{
+			// get te hDC of the target window
+			IntPtr hdcSrc = User32.GetWindowDC(handle);
+			// get the size
+			User32.RECT windowRect = new User32.RECT();
+			User32.GetWindowRect(handle, ref windowRect);
+			int width = windowRect.right - windowRect.left;
+			int height = windowRect.bottom - windowRect.top;
+			// create a device context we can copy to
+			IntPtr hdcDest = GDI32.CreateCompatibleDC(hdcSrc);
+			// create a bitmap we can copy it to,
+			// using GetDeviceCaps to get the width/height
+			IntPtr hBitmap = GDI32.CreateCompatibleBitmap(hdcSrc, width, height);
+			// select the bitmap object
+			IntPtr hOld = GDI32.SelectObject(hdcDest, hBitmap);
+			// bitblt over
+			GDI32.BitBlt(hdcDest, 0, 0, width, height, hdcSrc, 0, 0, GDI32.SRCCOPY);
+			// restore selection
+			GDI32.SelectObject(hdcDest, hOld);
+			// clean up 
+			GDI32.DeleteDC(hdcDest);
+			User32.ReleaseDC(handle, hdcSrc);
+			// get a .NET image object for it
+			Image img = Image.FromHbitmap(hBitmap);
+			// free up the Bitmap object
+			GDI32.DeleteObject(hBitmap);
+			return img;
+		}
 		private void selectActiveWindow_Click(object sender, RoutedEventArgs e)
         {
+			
+
 			Process[] process = Process.GetProcessesByName("devenv");
 
-			IntPtr hdcSrc = User32.GetWindowDC(process[0].Handle);
+			Image img = CaptureWindow(process[0].Handle);
+
 			// get the size
 			User32.RECT windowRect = new User32.RECT();
 			User32.GetWindowRect(process[0].Handle, ref windowRect);
@@ -161,6 +222,8 @@ namespace ScreenOCR
 				{
 					ImageProcessor.SetContrast(bitmap, (int)slider.Value);
 					image.Source = ImageProcessor.ImageSourceFromBitmap(bitmap);
+
+					//image.Source = System.Windows.Interop.Imaging.fr( img);
 					DoOCR(bitmap);
 				}
 			}
